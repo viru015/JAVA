7e58bdc9fba6bb937e1d40881352c923f244ad52
viru015 <ashirvadsingh015@gmail.com>
2026-09-13 02:40:46 +0530

    Updated rock paper scissor game

--- Mini Projects/ROCK PAPER SCISSORS GAME/src/Main.java
+++ Mini Projects/ROCK PAPER SCISSORS GAME/src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Random;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args){
+
+        // ROCK PAPER SCISSORS GAME
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] choices = {"rock", "paper", "scissors"};
+        String playerChoice;
+        String computerChoice;
+        String playAgain = "yes";
+
+        do{
+            System.out.print("Enter your move (rock, paper, scissors): ");
+            playerChoice = scanner.nextLine().toLowerCase();
+
+            if(!playerChoice.equals("rock") && !playerChoice.equals("paper") && !playerChoice.equals("scissors")){
+                System.out.println("Invalid choice");
+                continue;
+            }
+
+            computerChoice = choices[random.nextInt(3)];
+            System.out.println("Computer choice: " + computerChoice);
+
+            if(playerChoice.equals(computerChoice)){
+                System.out.println("It's a tie!");
+            }
+            else if((playerChoice.equals("rock") && computerChoice.equals("scissors")) ||
+                    (playerChoice.equals("paper") && computerChoice.equals("rock")) ||
+                    (playerChoice.equals("scissors") && computerChoice.equals("paper"))){
+                System.out.println("You win!");
+            }
+            else{
+                System.out.println("You lose!");
+            }
+
+            System.out.print("Play again (yes/no): ");
+            playAgain = scanner.nextLine().toLowerCase();
+
+        }while(playAgain.equals("yes"));
+
+        System.out.println("Thanks for playing!");
+
+        scanner.close();
     }
 }
